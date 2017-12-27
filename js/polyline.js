ed3813100da5bcb7bedbf5c7e9fdbee3b5408b01
haoxl3 <1098262516@qq.com>
2017-12-27 22:51:48 +0800

    画折线上的点

--- js/polyline.js
+++ js/polyline.js
@@ -1,7 +1,7 @@
 var H5ComponentPolyline = function(name, cfg){
     var component = new H5ComponentBase(name, cfg)
 
-    // 绘制网格线
+    // 绘制网格线-背景
     var w = cfg.width;
     var h = cfg.height;
 
@@ -10,6 +10,7 @@ var H5ComponentPolyline = function(name, cfg){
     var ctx = cns.getContext('2d');
     cns.width = ctx.width = w;
     cns.height = ctx.height = h;
+    component.append(cns);
 
     // 水平网格线 100份－> 10份
     var step = 10;
@@ -31,7 +32,32 @@ var H5ComponentPolyline = function(name, cfg){
         ctx.lineTo(x, h);
     }
     ctx.stroke();
-    
+
+    // 绘制折线数据
+    var cns = document.createElement('canvas');
+    var ctx = cns.getContext('2d');
+    cns.width = ctx.width = w;
+    cns.height = ctx.height = h;
     component.append(cns);
+
+    // 绘制折线数据
+    ctx.beginPath();
+    ctx.lineWidth = 3;
+    ctx.strokeStyle = '#ff8878';
+
+    var x = 0;
+    var y = 0;
+    var row_w = (w / (cfg.data.length + 1));
+    // 画点
+    for( i in cfg.data) {
+        var item = cfg.data[i];
+        x = row_w * i + row_w;
+        y = h * (1 - item[1]);
+        ctx.moveTo(x, y);
+        // 参数三：半径，参数四：起始弧度，参数五：结束弧底
+        ctx.arc(x, y, 5, 0, 2*Math.PI);
+    }
+    ctx.stroke();
+
     return component;
 }
