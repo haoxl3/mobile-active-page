81562923ffe0f1be5219306f0e950d59f51d3205
haoxl3 <1098262516@qq.com>
2017-12-28 21:12:51 +0800

    折线图动画

--- js/polyline.js
+++ js/polyline.js
@@ -50,50 +50,81 @@ var H5ComponentPolyline = function(name, cfg){
     cns.height = ctx.height = h;
     component.append(cns);
 
-    // 绘制折线数据
-    ctx.beginPath();
-    ctx.lineWidth = 3;
-    ctx.strokeStyle = '#ff8878';
+    /*
+    * 绘制折线以及对应的数据和阴影
+    * @param {float} per 0到1之间的数据，会根据这个值绘制最终数据对应的中间状态
+    * @return {DOM} Component元素
+    * */
+    var draw = function(per) {
+        //刚开始要清空画布
+        ctx.clearRect(0,0,w,h);
+        // 绘制折线数据
+        ctx.beginPath();
+        ctx.lineWidth = 3;
+        ctx.strokeStyle = '#ff8878';
 
-    var x = 0;
-    var y = 0;
-    var row_w = (w / (cfg.data.length + 1));
-    // 画点
-    for( i in cfg.data) {
-        var item = cfg.data[i];
-        x = row_w * i + row_w;
-        y = h * (1 - item[1]);
-        ctx.moveTo(x, y);
-        // 参数三：半径，参数四：起始弧度，参数五：结束弧底
-        ctx.arc(x, y, 5, 0, 2*Math.PI);
-    }
-    // 连线
-    // 移动画笔到第一个数据的点位置
-    ctx.moveTo(row_w, h*(1 - cfg.data[0][1]));
-    for(i in cfg.data) {
-        var item = cfg.data[i];
-        x = row_w * i + row_w;
-        y = h * (1-item[1]);
-        ctx.lineTo(x, y);
-    }
-    ctx.stroke();
+        var x = 0;
+        var y = 0;
+        var row_w = (w / (cfg.data.length + 1));
+        // 画点
+        for (i in cfg.data) {
+            var item = cfg.data[i];
+            x = row_w * i + row_w;
+            y = h-(item[1]*h*per);
+            ctx.moveTo(x, y);
+            // 参数三：半径，参数四：起始弧度，参数五：结束弧底
+            ctx.arc(x, y, 5, 0, 2 * Math.PI);
+        }
+        // 连线
+        // 移动画笔到第一个数据的点位置
+        ctx.moveTo(row_w, h-(cfg.data[0][1]*h*per) );
+        for (i in cfg.data) {
+            var item = cfg.data[i];
+            x = row_w * i + row_w;
+            y = h-(item[1]*h*per);
+            ctx.lineTo(x, y);
+        }
+        ctx.stroke();
 
-    ctx.lineWidth = 1;
-    ctx.strokeStyle = "rgba(255, 136, 120)";
-    //绘制阴影
-    ctx.lineTo(x, h);
-    ctx.lineTo(row_w,h);
-    ctx.fillStyle = 'rgba(255, 136, 120, 0.2)';
-    ctx.fill();
+        ctx.lineWidth = 1;
+        ctx.strokeStyle = "rgba(255, 136, 120)";
+        //绘制阴影
+        ctx.lineTo(x, h);
+        ctx.lineTo(row_w, h);
+        ctx.fillStyle = 'rgba(255, 136, 120, 0.2)';
+        ctx.fill();
 
-    // 写数据
-    for(i in cfg.data) {
-        var item = cfg.data[i];
-        x = row_w * i + row_w;
-        y = h * (1-item[1]);
-        ctx.fillStyle = item[2] ? item[2] : '#595959';//item[2]为字体颜色
-        ctx.fillText(((item[1]*100)>>0) + '%', x-10, y-10);
+        // 写数据
+        for (i in cfg.data) {
+            var item = cfg.data[i];
+            x = row_w * i + row_w;
+            y = h-(item[1]*h*per);
+            ctx.fillStyle = item[2] ? item[2] : '#595959';//item[2]为字体颜色
+            ctx.fillText(((item[1] * 100) >> 0) + '%', x - 10, y - 10);
+        }
+        ctx.stroke();
     }
+    //draw(1);
+    //折现生长动画
+    component.on('onLoad', function(){
+        var s = 0;
+        for(i=0;i<100;i++){
+            setTimeout(function(){
+                s+=.01;
+                draw(s);
+            },i*10 + 500);//时间递增,500为网格线入场时间
+        }
+    });
+    //折线退场动画
+    component.on('onLeave', function(){
+        var s = 1;
+        for(i=0;i<100;i++){
+            setTimeout(function(){
+                s -=.01;
+                draw(s);
+            },i*10);//时间递增
+        }
+    });
 
     return component;
 }
