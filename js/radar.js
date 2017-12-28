887d0c2f50de21ce90f504887a8e69914b8cf86f
haoxl3 <1098262516@qq.com>
2017-12-28 22:55:07 +0800

    雷达图动画

--- js/radar.js
+++ js/radar.js
@@ -46,23 +46,84 @@ var H5ComponentRadar = function(name, cfg){
         var y = r + Math.cos(rad)*r;
         ctx.moveTo(r,r);
         ctx.lineTo(x,y);
+        // 输出项目文字
+        var text = $('<div class="text"></div>');
+        text.text(cfg.data[i][0]);
+        //文字挨个延迟
+        text.css('transition','all .5s ' + i *.1 + 's');
+
+        //文字位置
+        if(x > w/2){
+            text.css('left',x/2+5);
+        }else{
+            text.css('right',(w-x)/2+5);
+        }
+        if(y>h/2){
+            text.css('top',y/2+5);
+        }else{
+            text.css('bottom',(h-y)/2+5);
+        }
+        if(cfg.data[i][2]){
+            text.css('color',cfg.data[i][2]);
+        }
+        text.css('opacity',0);//文字先隐藏
+        component.append(text);
     }
     ctx.strokeStyle = '#e0e0e0';
     ctx.stroke();
 
+
+
+    //数据层开发，加入一个画布
+    var cns = document.createElement('canvas');
+    var ctx = cns.getContext('2d');
+    cns.width = ctx.width = w;
+    cns.height = ctx.height = h;
+    component.append(cns);
     /*
      * */
+    cns.strokeStyle = "#f00000";
     var draw = function(per) {
+        //当图加载完后再显示文字
+        if(per >= 1){
+            component.find('.text').css('opacity',1);
+        }
+        if(per <1) {
+            component.find('.text').css('opacity',0);
+        }
+        ctx.clearRect(0,0,w,h);
+        //输出数据的折线
+        for(var i=0;i<step;i++){
+            var rad = (2*Math.PI / 360)*(360 / step) * i
+            var rate = cfg.data[i][1]*per;
+            var x = r + Math.sin(rad)*r*rate;
+            var y = r + Math.cos(rad)*r*rate;
+            ctx.lineTo(x,y);
+        }
+        ctx.closePath();
+        ctx.stroke();
 
+        //输出数据的点
+        ctx.fillStyle = '#ff7676';
+        for(var i=0;i<step;i++){
+            var rad = (2*Math.PI / 360)*(360 / step) * i
+            var rate = cfg.data[i][1]*per;
+            var x = r + Math.sin(rad)*r*rate;
+            var y = r + Math.cos(rad)*r*rate;
+            ctx.beginPath();
+            ctx.arc(x,y,5,0,2*Math.PI);
+            ctx.fill();
+            ctx.closePath();
+        }
     }
-    //draw(1);
+
     //雷达图生长动画
     component.on('onLoad', function(){
         var s = 0;
         for(i=0;i<100;i++){
             setTimeout(function(){
                 s+=.01;
-                //draw(s);
+                draw(s);
             },i*10 + 500);//时间递增,500为网格线入场时间
         }
     });
@@ -72,7 +133,7 @@ var H5ComponentRadar = function(name, cfg){
         for(i=0;i<100;i++){
             setTimeout(function(){
                 s -=.01;
-                //draw(s);
+                draw(s);
             },i*10);//时间递增
         }
     });
